53e386c1f1705bd1d817a68b3385fe27f8472408
Empowerdeith <clone10700@hotmail.com>
2021-05-25 19:14:32 -0400

    F

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,21 @@
 function prueba(){
-	let url="34.233.135.60/api/boleta/12345"
+	/*let url="34.233.135.60/api/boleta/12345"
 	fetch(url)
 		.then(response => response.json())
 		.then(data => console.log(data))
-		.catch(error => console.log(error))
+		.catch(error => console.log(error))*/
 
-	/*var buscar1 = document.getElementById('id_buscar').value.toString();
-	var formdata = new FormData();
+	/*var buscar1 = document.getElementById('id_buscar').value.toString();*/
+	//var formdata = new FormData();
 
 	var requestOptions = {
 	  method: 'GET',
-	  body: formdata,
+	  //body: formdata,
 	  redirect: 'follow'
 	};
 
-	fetch("34.233.135.60/api/boleta/"+buscar1, requestOptions)
+	fetch("34.233.135.60/api/boleta/12345", requestOptions)
 	  .then(response => response.text())
 	  .then(result => console.log(result))
-	  .catch(error => console.log('error', error));*/
+	  .catch(error => console.log('error', error));
 }
