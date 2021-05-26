c6e391ffb8eec6b675fde0b2c37e9153a798b6ba
Empowerdeith <clone10700@hotmail.com>
2021-05-25 21:56:19 -0400

    Test12

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,4 @@
-function prueba(){
-	/*let url="http://34.233.135.60/api/boleta/12345"
-	fetch(url)
-		.then(response => response.json())
-		.then(data => console.log(data))
-		.catch(error => console.log(error))*/
-
+/*function prueba(){
 	var buscar1 = document.getElementById('id_buscar').value.toString();
 	var requestOptions = {
 	  method: 'GET',
@@ -15,42 +9,25 @@ function prueba(){
 	  .then(response => response.text())
 	  .then(result => console.log(result))
 	  .catch(error => console.log('error', error));
-}
+}*/
 
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const boletaContainer = document.querySelector('.boleta-container');
-function TraerBoleta() {
-	fetch("http://34.233.135.60/api/boleta/12345")
-	.then(res => res.json())
-	.then(data => {
-		crearboleta(data);
-	})
 
-}
 
-function crearboleta(data){
-	const h3 = document.createElement('h3');
-
-	h3.textContent = data.region;
-
-	const div = document.createElement('div');
-	div.appendChild(h3);
-	boletaContainer.appendChild(div);
+function prueba(){
+	var buscar1 = document.getElementById('id_buscar').value.toString();
+	let url="http://34.233.135.60/api/boleta/"+buscar1
+	fetch(url)
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.log(error))
 
 }
-
-TraerBoleta();
-
-
-/*function prueba(){
-
-}
-function appendData(data) {
+/*function appendData(data) {
   var mainContainer = document.getElementById("myData");
   for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
     div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
     mainContainer.appendChild(div);
   }
-}*/
+}*/	
+/**/
