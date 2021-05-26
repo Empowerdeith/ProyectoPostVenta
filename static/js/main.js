11d1e698039e7b096598a76c8cfef3c033f365f0
Empowerdeith <clone10700@hotmail.com>
2021-05-25 21:00:17 -0400

    plz funciona 2

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ function prueba(){
 		.then(data => console.log(data))
 		.catch(error => console.log(error))*/
 
-	/*var buscar1 = document.getElementById('id_buscar').value.toString();*/
+	var buscar1 = document.getElementById('id_buscar').value.toString();
 	//var formdata = new FormData();
 
 	var requestOptions = {
@@ -14,7 +14,7 @@ function prueba(){
 	  redirect: 'follow'
 	};
 
-	fetch("http://34.233.135.60/api/boleta/12345", requestOptions)
+	fetch("http://34.233.135.60/api/boleta/"+buscar1, requestOptions)
 	  .then(response => response.text())
 	  .then(result => console.log(result))
 	  .catch(error => console.log('error', error));
