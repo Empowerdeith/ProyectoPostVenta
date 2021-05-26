1345f47422f81591ba03ad678b4ac5eee22c8708
Empowerdeith <clone10700@hotmail.com>
2021-05-26 15:09:50 -0400

    cambios a html

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,3 @@
-/*function prueba(){
-	var buscar1 = document.getElementById('id_buscar').value.toString();
-	var requestOptions = {
-	  method: 'GET',
-	  redirect: 'follow'
-	};
-
-	fetch("http://34.233.135.60/api/boleta/"+buscar1, requestOptions)
-	  .then(response => response.text())
-	  .then(result => console.log(result))
-	  .catch(error => console.log('error', error));
-}*/
-
-
-
-function prueba(){
-	var buscar1 = document.getElementById('id_buscar').value.toString();
-	let url="http://18.207.25.202/api/boleta/"+buscar1
-	fetch(url)
-	.then(response => response.json())
-	.then(data => console.log(data))
-	.catch(error => console.log(error))
-}
-/*function prueba(){
-	var buscar1 = document.getElementById('id_buscar').value.toString();
-	let url="http://34.233.135.60/api/boleta/"+buscar1
-	fetch(url)
-	.then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-            	console.log(data);
-                mostrarBoleta(data);
-            })
-            .catch(function (err) {
-                console.log('error: ' + err);
-            });
-}
-
 function mostrarBoleta(data){
 	var mainContainer = document.getElementById("myData");
   	for (var i = 0; i < data.length; i++) {
@@ -50,4 +11,14 @@ function mostrarBoleta(data){
 		div.innerHTML = 'Total: ' + data[i].total;
 		mainContainer.appendChild(div);
 	}
-}*/
+}
+
+function prueba(){
+	var buscar1 = document.getElementById('id_buscar').value.toString();
+	let url="http://18.207.25.202/api/boleta/"+buscar1
+	fetch(url)
+	.then(response => response.json())
+	.then(data => mostrarBoleta(data))
+	.catch(error => console.log(error))
+}
+
