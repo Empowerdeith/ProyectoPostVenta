db3e37549ec390a63283feaf3c8e2d02f08e7bd6
Empowerdeith <clone10700@hotmail.com>
2021-05-26 17:01:51 -0400

    123

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,28 @@
 function mostrarBoleta(data){
 	var mainContainer = document.getElementById("myData");
 	var div = document.createElement("div");
-	div.innerHTML = 'Id: ' + data.id+'<br>';
-	div.innerHTML += 'Número Boleta: ' + data.numero_boleta.value+'<br>';
+	div.innerHTML = 'Número Boleta: ' + data.numero_boleta+'<br>';
 	div.innerHTML += 'Comprador: ' + data.Comprador+'<br>';
 	div.innerHTML += 'Fecha de orden: ' + data.date_ordered+'<br>';
 	div.innerHTML += 'Producto: ' + data.producto+'<br>';
 	div.innerHTML += 'Cantidad: ' + data.cantidad+'<br>';
 	div.innerHTML += 'Total: ' + data.total+'<br>';
+	div.innerHTML += 'Región: ' + data.region+'<br>';
+	div.innerHTML += 'Ciudad: ' + data.ciudad+'<br>';
+	div.innerHTML += 'Dirección: ' + data.direccion+'<br>';
 	mainContainer.appendChild(div);
 }
 //console.log(data)
 function prueba(){
 	var buscar1 = document.getElementById('id_buscar').value.toString();
-	let url="http://18.207.25.202/api/boleta/"+buscar1
-	fetch(url)
-	.then(response => response.json())
-	.then(data => mostrarBoleta(data))
-	.catch(error => console.log(error))
+	if(!buscar1.isEmpty()){
+		let url="http://18.207.25.202/api/boleta/"+buscar1
+		fetch(url)
+		.then(response => response.json())
+		.then(data => mostrarBoleta(data))
+		.catch(error => console.log(error))
+	}
+	else{
+		window.alert("Debe ingresar un número de transacción.")
+	}
 }
