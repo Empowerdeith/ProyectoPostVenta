645cfe06bce97acacb7ed5adef3514781a8ee787
Empowerdeith <clone10700@hotmail.com>
2021-06-05 21:37:58 -0400

    test

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ function hide_table() {
   $( "#myData" ).hide();
 }
 setTimeout(hide_table, 40);
-function mostrarBoleta(data){
+/*function mostrarBoleta(data){
 	if (data == "No existe la boleta ingresada."){
 		hide_table()
 		$("#error_msg").html("<br><br>"+data);
@@ -36,11 +36,28 @@ function mostrarBoleta(data){
 		ciudad.html(data.ciudad);
 		direccion.html(data.direccion);
 	}
-}
+}*/
 Object.prototype.isEmpty = function () {
     return Object.keys(this).length == 0;
 }
+
 function prueba(){
+	var buscar1 = $('#id_buscar').val().toString();
+	// console.log(buscar1);
+	if(!buscar1.isEmpty()){
+		let url="http://3.83.24.216/Cliente/"+buscar1
+		fetch(url)
+		.then(response => response.json())
+		.then(data => console.log(data))
+		//.then(data => mostrarBoleta(data))
+		.catch(error => console.log(error))
+	}
+	else{
+		hide_table()
+		$("#error_msg").html("<br><br>Debe ingresar un número de transacción.");
+	}
+}
+/*function prueba(){
 	var buscar1 = $('#id_buscar').val().toString();
 	// console.log(buscar1);
 	if(!buscar1.isEmpty()){
@@ -54,7 +71,7 @@ function prueba(){
 		hide_table()
 		$("#error_msg").html("<br><br>Debe ingresar un número de transacción.");
 	}
-}
+}*/
 function selectall(){
 	$( "#table_checkbox").find( "input" ).prop('checked', true);
 }
