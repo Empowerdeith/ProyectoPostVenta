cac0baa0e6c015da5b207ce1f647f773d2d10977
Empowerdeith <clone10700@hotmail.com>
2021-06-12 17:08:55 -0400

    Cleaning database

--- api_post_venta/admin.py
+++ api_post_venta/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-"""admin.site.register(Customer)
-admin.site.register(Producto)
-admin.site.register(Orden)
-admin.site.register(OrdenItem)
-admin.site.register(ShippingAddress)"""
 admin.site.register(Boleta)
