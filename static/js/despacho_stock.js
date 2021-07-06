5e3d4a619e1a7a65c43a8a51654196af9be8a556
Empowerdeith <clone10700@hotmail.com>
2021-07-06 18:12:08 -0400

    test error msg

--- static/js/despacho_stock.js
+++ static/js/despacho_stock.js
@@ -149,8 +149,9 @@ function enviar_orden_retiro(){
 	}
 }
 function clean_retiro_a_despacho(){
-	document.getElementById('#rut_1').value = '';
+	//document.getElementById('#rut_1').value = '';
 	//var rut_ret = $('#rut_1').val();
+	$('#rut_1').val("");
 	var nom_ret = $('#nombre_cl_1').val();
 	var tel_ret = $('#num_telf_1').val();
 	var email_ret = $('#email_1').val();
